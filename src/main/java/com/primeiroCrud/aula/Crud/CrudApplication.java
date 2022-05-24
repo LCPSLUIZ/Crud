8e35e316df18d6ea1ef58d1c853ecdb94a9651c3
Luiz Santos <luizclaudio072@gmail.com>
2022-05-24 00:14:04 -0300

    SELECT DONE

--- src/main/java/com/primeiroCrud/aula/Crud/CrudApplication.java
+++ src/main/java/com/primeiroCrud/aula/Crud/CrudApplication.java
@@ -1,13 +1,26 @@
 package com.primeiroCrud.aula.Crud;
 
+import com.primeiroCrud.aula.Crud.Model.SelectConsult;
+import com.primeiroCrud.aula.Crud.Repository.ConnectionDataBase;
+import com.primeiroCrud.aula.Crud.Repository.Exercicio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
 public class CrudApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CrudApplication.class, args);
+	public static void main(String[] args) throws SQLException{
+		SelectConsult consult = new SelectConsult();
+
+		Exercicio exercicioSelect = consult.Select(1);
+		System.out.println(exercicioSelect);
+
+		exercicioSelect = consult.Select("Calculo 4");
+		System.out.println(exercicioSelect);
+
 	}
 
 }
---
+++ src/main/java/com/primeiroCrud/aula/Crud/Repository/ConnectionDataBase.java
@@ -0,0 +1,30 @@
+package com.primeiroCrud.aula.Crud.Repository;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ConnectionDataBase {
+    private static String hostName = "jdbc:postgresql://localhost/Exercicio";
+    private static String login = "postgres";
+    private static String password = "96787320";
+    static Connection connection = null;
+
+    public static Connection getConnection() throws SQLException{
+        try {
+            connection = DriverManager.getConnection(hostName, login, password);
+            System.out.println("Conectado com Sucesso! ");
+        } catch(SQLException e) {
+            System.out.println("Erro na conexão =(");
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
