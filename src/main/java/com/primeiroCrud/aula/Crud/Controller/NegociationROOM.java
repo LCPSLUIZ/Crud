318d2f566ce7d988e9f3db6a3c6a7021f532fbb6
Luiz interprise PC <luiz.santos@linkedrh.com.br>
2022-05-30 12:44:09 -0300

    Refatoring code

--- src/main/java/com/primeiroCrud/aula/Crud/Controller/NegociationROOM.java
+++ src/main/java/com/primeiroCrud/aula/Crud/Controller/NegociationROOM.java
@@ -5,10 +5,14 @@ import com.primeiroCrud.aula.Crud.DAO.ExercicioDAO.InsertConsult;
 import com.primeiroCrud.aula.Crud.DAO.ExercicioDAO.SelectConsult;
 import com.primeiroCrud.aula.Crud.DAO.ExercicioDAO.UpdateConsult;
 import com.primeiroCrud.aula.Crud.Model.Exercicio;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 import java.util.Scanner;
 
+@Getter
+@Setter
 public class NegociationROOM {
     private final Scanner inputInt = new Scanner(System.in);
     private final Scanner inputName = new Scanner(System.in);
@@ -32,86 +36,84 @@ public class NegociationROOM {
 
             option = inputInt.nextInt();
 
-            if(option == 1) {
-                SelectConsult consult = new SelectConsult();
-                List<Exercicio> SelectAllTable = consult.Select();
+            optionPanel(option);
 
-                System.out.println("Toda a Tabela: ");
-                SelectAllTable.forEach(System.out::println);
-
-            } else if(option == 2) {
-                System.out.println("Qual o numero do exercicio que você quer ver? ");
-                setExerciceNumber(inputInt.nextInt());
-
-                SelectConsult consult = new SelectConsult();
-                Exercicio exercicioSelect = consult.Select(getExerciceNumber());
-                System.out.println(exercicioSelect);
-
-            } else if (option == 3) {
-                System.out.println("Qual registro você quer atualizar? ");
-                setExerciceNumber(inputInt.nextInt());
-
-                System.out.println("Qual será o novo nome? ");
-                setNewName(inputName.nextLine());
-
-                System.out.println("Qual será a nova dificuldade? ");
-                setNewDificult(inputDificult.nextLine());
-
-                UpdateConsult Update = new UpdateConsult();
-                Update.UpdateMethod(getNewName(), getNewDificult(), getExerciceNumber());
-
-            } else if (option == 4) {
-                System.out.println("Qual o nome do novo registro? ");
-                setNewName(inputName.nextLine());
-
-                System.out.println("Qual será a dificuldade? ");
-                setNewDificult(inputDificult.nextLine());
-
-                InsertConsult Insert = new InsertConsult();
-                Insert.InsertInto(getNewName(), getNewDificult());
-
-            } else if (option == 5) {
-                SelectConsult consult = new SelectConsult();
-                List<Exercicio> SelectAllTable = consult.Select();
-
-                System.out.println("Toda a Tabela: ");
-                SelectAllTable.forEach(System.out::println);
-
-                System.out.println("Qual o numero do exercicio que você deseja apagar? ");
-                setExerciceNumber(inputInt.nextInt());
-                DeleteConsult Delete = new DeleteConsult();
-                Delete.DeleteMethod(getExerciceNumber());
-            } else {
-                System.out.println("Saindo da tela de opções...");
-                System.out.println("\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n  " +
-                        "\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n " +
-                        "\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n ");
-            }
-        } while(option > 0 && option < 6);
+        } while(option >0 && option< 6);
         System.out.println("Finalizando aplicação ...");
     }
+    
+    private void option1(){
+        SelectConsult consult = new SelectConsult();
+        List<Exercicio> SelectAllTable = consult.Select();
 
-    private String getNewName(){
-        return newName;
+        System.out.println("Toda a Tabela: ");
+        SelectAllTable.forEach(System.out::println);
+    }
+    
+    private void option2(){
+        System.out.println("Qual o numero do exercicio que você quer ver? ");
+        setExerciceNumber(inputInt.nextInt());
+
+        SelectConsult consult = new SelectConsult();
+        Exercicio exercicioSelect = consult.Select(getExerciceNumber());
+        System.out.println(exercicioSelect);
+    }
+    
+    private void option3(){
+        System.out.println("Qual registro você quer atualizar? ");
+        setExerciceNumber(inputInt.nextInt());
+
+        System.out.println("Qual será o novo nome? ");
+        setNewName(inputName.nextLine());
+
+        System.out.println("Qual será a nova dificuldade? ");
+        setNewDificult(inputDificult.nextLine());
+
+        UpdateConsult Update = new UpdateConsult();
+        Update.UpdateMethod(getNewName(), getNewDificult(), getExerciceNumber());
+    }
+    
+    private void option4(){
+        System.out.println("Qual o nome do novo registro? ");
+        setNewName(inputName.nextLine());
+
+        System.out.println("Qual será a dificuldade? ");
+        setNewDificult(inputDificult.nextLine());
+
+        InsertConsult Insert = new InsertConsult();
+        Insert.InsertInto(getNewName(), getNewDificult());
+
+    }
+    
+    private void option5(){
+        SelectConsult consult = new SelectConsult();
+        List<Exercicio> SelectAllTable = consult.Select();
+
+        System.out.println("Toda a Tabela: ");
+        SelectAllTable.forEach(System.out::println);
+
+        System.out.println("Qual o numero do exercicio que você deseja apagar? ");
+        setExerciceNumber(inputInt.nextInt());
+        DeleteConsult Delete = new DeleteConsult();
+        Delete.DeleteMethod(getExerciceNumber());
     }
 
-    private void setNewName(String newName){
-        this.newName = newName;
-    }
-
-    private String getNewDificult(){
-        return newDificult;
-    }
-
-    private void setNewDificult(String newDificult){
-        this.newDificult = newDificult;
-    }
-
-    private int getExerciceNumber(){
-        return exerciceNumber;
-    }
-
-    private void setExerciceNumber(int exerciceNumber){
-        this.exerciceNumber = exerciceNumber;
+    private void optionPanel(int option) {
+        if(option == 1) {
+            option1();
+        } else if(option == 2) {
+            option2();
+        } else if (option == 3) {
+            option3();
+        } else if (option == 4) {
+            option4();
+        } else if (option == 5) {
+            option5();
+        } else {
+            System.out.println("Saindo da tela de opções...");
+            System.out.println("\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n  " +
+                    "\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n " +
+                    "\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n ");
+        }
     }
 }
